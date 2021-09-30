0c67d75cf77e678702a313bda2ab25f234df6a26
gochuicod <gochuicodarelleq@gmail.com>
2021-09-30 18:00:38 +0800

    Windows Forms Program Version 2.0

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 ## This is my development branch
 ### Language Used: C#
-- Committed and Pushed Program 1 (Struct)
-- Committed and Pushed Program 2 (Windows Forms)
+
+#### September 9, 2021
+- Program 1 (Struct)
+
+#### September 12, 2021
+- Program 2 (Windows Forms)
+
+#### September 30, 2021
+- Program 3 (Windows Forms Version 2.0)
---
+++ WinFormsApp1/WinFormsApp1/Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public string[] studentData = new string[20];
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            string sample = string.Join("", studentData);
+            richTextBox1.Text = sample;
+        }
+    }
+}
