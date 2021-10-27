7e65486ca057eea42741b7011150af1c41b0a0ea
sontormis <90128505+sontormis@users.noreply.github.com>
2021-10-27 02:15:58 -0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExerWindowsApp
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student studentObject = new Student();
+            studentObject.Id = textBox1.Text;
+            studentObject.Firstname = textBox2.Text;
+            studentObject.Lastname = textBox3.Text;
+            studentObject.Quizscore = textBox4.Text;
+            studentObject.Remarks = textBox5.Text;
+           
+
+
+            listBoxResults.Items.Add(studentObject.Id);
+            listBoxResults.Items.Add(studentObject.Firstname);
+            listBoxResults.Items.Add(studentObject.Lastname);
+            listBoxResults.Items.Add(studentObject.Quizscore);
+            listBoxResults.Items.Add(studentObject.Remarks);
+
+           
+          
+        }
+
+       
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+namespace ExerWindowsApp
+{
+    public enum Remark : int
+
+
+    {
+        Poor = 1, Fair = 2, Good = 3, Exellent = 4
+
+    }
+
+
+    public class Student
+    {
+        
+        private string id;
+        private string firstname;
+        private string lastname;
+        private string quizscore;
+        private string remarks;
+
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+
+        }
+
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+
+        }
+
+        public string Quizscore
+        {
+            get { return quizscore; }
+            set { quizscore = value; }
+        }
+
+        public string Remarks
+        {
+            get { return remarks; }
+            set { remarks = value; }
+
+
+           
+        }
+            
+      }
+
+  }
+
